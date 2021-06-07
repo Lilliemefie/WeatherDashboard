cb2768c9b7f4f2313e9f086213610848895c5295
lillie <lillie@Lillies-MacBook-Pro.local>
2021-06-06 23:24:34 -0700

    added localStorage, created citySearch List

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 //create variable
 var searchBtn = document.querySelector('#searchBtn');
-var container = document.querySelector('.container-fluid');
 var fiveDayRowId = $('#fiveDayRow');
-
-//var searchInputVal;
+var cityListId = $('#cityListId');
+//set variablr for the current date (nmultiple Locale Support format)
+var today = moment().format('L');
 //API key from openweathermap (in my account)
 var key = '91982993508dc4b35a5d3a4295c7bf1e';
-// var lat;
-// var lon;
+
 
 
 // Search bar submit function
@@ -17,12 +16,21 @@ function handleSearchFormSubmit(event){
     var searchInputVal = document.querySelector('.searchInput').value;
     console.log(searchInputVal)
   
-
     if (!searchInputVal) {
       console.error('Please put the city...');
       return;
+    }else{
+      localStorage.setItem("searchInputVal", searchInputVal);
+      var searchCity = localStorage.getItem("searchInputVal");
+      console.log(searchCity);
+     
+      var cityList = document.createElement('li');
+      $(cityList).text(searchCity);
+      $('#cityListId').append(cityList);
+       
+
     }  
-    container.style.display = "block";
+    //call function
     getWeather(searchInputVal);
     getFive(searchInputVal);
   }
@@ -32,6 +40,18 @@ function handleSearchFormSubmit(event){
 searchBtn.addEventListener('click', handleSearchFormSubmit);
 
 
+//save searched city on left side
+// function saveCity(searchInputVal){
+//   var searchCity = localStorage.getItem("searchInputVal", searchInputVal);
+//   console.log(searchCity);
+  //var cityList = document.textContent(searchCity);
+
+  // var cityBtn = document.createElement('li');
+  // cityBtn.appendChild(cityList);
+  // cityListId.appendChild(cityBtn);
+
+
+// }
 
 
 
@@ -41,22 +61,20 @@ $.ajax({
     url: ('https://api.openweathermap.org/data/2.5/weather?q=' + searchInputVal + '&units=imperial' + '&appid=' + key),
     method: 'GET',
   }).then(function (response) {
-    
+    console.log(response);
+
+    //give variable to "lat" and "lon" to use for getting UV Index API 
     var lat = response.coord.lat;
     var lon = response.coord.lon;
-    console.log(lat);
-    console.log(lon);
   
-  
+    
     //set to display
-    $('#curCity').text(response.name);
-    $('#curTemp').text('Temperature: ' + response.main.temp + '°F');
+    $('#curCity').text('Current City:  ' + response.name + ' (' + today + ')');
+    $('#curTemp').text('Temperature: ' + response.main.temp + ' °F');
     $('#curWind').text('Wind: ' + response.main.humidity);
     $('#curHumi').text('Humidity: ' + response.wind.speed);
 
-    var today = moment().format('L');
-    $("#curDay").text(today);
-    console.log(today);
+    
 
     // the lat and lon here are from lines 38 and 39
     getUV(lat, lon);
@@ -67,6 +85,9 @@ $.ajax({
   });
 }
 
+
+
+//get UVI by using lat and lon parameter from line 38,39
 function getUV (lat, lon){
   $.ajax({
       url: ('https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + key),
@@ -91,14 +112,13 @@ function getUV (lat, lon){
         for (var i = 0; i < response.list.length; i++) {
       //  $(temp).text(response.list[i].main.temp);   
 
-      //  $
       
       fiveDayRowId.append(`<div class="col mb-4">
       <div class="card">
         <div class="card-body">
           <h5 class="card-title" id="dateFive">Card title</h5>
           <img src="https://img.icons8.com/fluent-systems-regular/2x/puzzle.png">
-          <p class="card-text temp">Temp: ${response.list[i].main.temp}</p>
+          <p class="card-text temp">Temp: ${response.list[i].main.temp}  °F</p>
           <p class="card-text wind">Wind: ${response.list[i].wind.speed}</p>
           <p class="card-text humi">Humidity:${response.list[i].main.humidity}</p>
         </div>
@@ -115,24 +135,6 @@ function getUV (lat, lon){
       });
     }
 
-  // function printFive(){
-
-  // } 
-
-
-// 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + key),
-
-// function getUV (){
-// $.ajax({
-//   uvApiUrl: ('https://api.openweathermap.org/data/2.5/uvi?lat=' + lat + '&lon=' + lon + '&appid=' + key),
-//   method: 'GET',
-// }).then(function (response) {
-//   var lat = response.coord.lat;
-//   var lon = response.coord.lon;
-//   console.log(response);
-//     return response.json()
-
-//   })
-
+ 
 
 
